40985bcc34de2ff9336585f7bdd1aaa499d1bedd
mr-siro <nhatmoc97@gmail.com>
2025-03-07 16:24:06 +0700

    update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './user/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-
+import * as path from 'path';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: parseInt(configService.get<string>('DB_PORT') || '3306', 10),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [User],
-        migrations: ['dist/migrations/*.js'],
-        autoLoadEntities: true,
-        synchronize: false,
-      }),
+      useFactory: (configService: ConfigService) => {
+        const entitiesPath = path.join(__dirname, '**/*.entity{.ts,.js}');
+
+        return {
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST'),
+          port: parseInt(configService.get<string>('DB_PORT') ?? '3306', 10),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [entitiesPath],
+          migrations: ['dist/migrations/*.js'],
+          autoLoadEntities: true,
+          synchronize: false,
+          logging: 'all',
+        };
+      },
     }),
     UserModule,
   ],
--- src/data-source.ts
+++ src/data-source.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST ?? 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'pass',
-  database: process.env.DB_NAME || 'exam1',
+  username: process.env.DB_USER ?? 'root',
+  password: process.env.DB_PASSWORD ?? 'pass',
+  database: process.env.DB_NAME ?? 'exam1',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   synchronize: false,
